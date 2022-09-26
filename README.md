abbf1b6d21c52a80a4769980380d9c30ff66922b
Andy Miller <rhuk@mac.com>
2022-09-26 17:27:56 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# MailerSend Plugin
+
+A plugin for sending emails via forms using the MailerSend API.
+
+The **MailerSend** Plugin is an extension for [Grav CMS](https://github.com/getgrav/grav).
+
+## Installation
+
+Installing the MailerSend plugin can be done in one of three ways: The GPM (Grav Package Manager) installation method lets you quickly install the plugin with a simple terminal command, the manual method lets you do so via a zip file, and the admin method lets you do so via the Admin Plugin.
+
+### GPM Installation (Preferred)
+
+To install the plugin via the [GPM](https://learn.getgrav.org/cli-console/grav-cli-gpm), through your system's terminal (also called the command line), navigate to the root of your Grav-installation, and enter:
+
+    bin/gpm install mailersend
+
+This will install the MailerSend plugin into your `/user/plugins`-directory within Grav. Its files can be found under `/your/site/grav/user/plugins/mailersend`.
+
+## Configuration
+
+### Main plugin configuration options:
+
+```yaml
+enabled: true
+api_token: <YOUR API TOKEN>
+defaults:
+  to: 
+  from: 
+  cc:
+  bcc:
+```
+
+NOTE: You can create a custom permission token, but it at least needs full **Email** permissions.
+
+### Form definition Example:
+
+```yaml
+    process:
+        mailersend:
+          subject: "[WEB Form] {{ form.value.name|e }}" # REQUIRED
+          reply_to: ["{{ form.value.email }}", "{{ form.value.name|e }}"] # REQUIRED
+          from: ["no-reply@yoursite.com", "Your Name"] # OPTIONAL: can use defaults if provided
+          bcc: ["{{ form.value.email }}", "{{ form.value.name|e }}"] # OPTIONAL: can use defaults if provided
+          to: ["contact@yoursite.com", "Your Name"] # OPTIONAL: can use defaults if provided
+          html: true # OPTIONAL: will assume message is HTML and create a text version automatically
+          message: "{{ form.value.message }}" # OPTIONAL: will use form.value.message if not set
+          template_id: jy7zpl98ye345vx6 # OPTIONAL: will use this template ID if specified and messages is ignored
+          substitutions: # OPTIONAL: only used when template_id is provided
+            name: "{{ form.value.name|e }}"
+            email: "{{ form.value.email }}"
+            message: "{{ form.value.message|e }}"
+```
+
+You can use any value from the form via `{{ form.value.<field-attribute> }}` or from Grav's configuration via `{{ config.x.y.z }}`
+
+#### YAML Email address formats: 
+
+preferred: `[hello@yoursite.com, Your Name]`
+alternative: `{email: hello@yoursite.com, name: Your Name}`
+discouraged but works: `hello@yoursite.com`
+
+
---
+++ mailersend.php
@@ -0,0 +1,181 @@
+<?php
+namespace Grav\Plugin;
+
+use Composer\Autoload\ClassLoader;
+use Grav\Common\Plugin;
+use Grav\Common\Utils;
+use League\HTMLToMarkdown\HtmlConverter;
+use MailerSend\Exceptions\MailerSendException;
+use MailerSend\Helpers\Builder\EmailParams;
+use MailerSend\Helpers\Builder\Recipient;
+use MailerSend\Helpers\Builder\Variable;
+use MailerSend\MailerSend;
+use Psr\Http\Client\ClientExceptionInterface;
+use RocketTheme\Toolbox\Event\Event;
+use Symfony\Component\HttpClient\Exception\JsonException;
+
+/**
+ * Class MailerSendPlugin
+ * @package Grav\Plugin
+ */
+class MailerSendPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+            'onFormProcessed'      => ['onFormProcessed', 0],
+        ];
+    }
+
+    /**
+     * Composer autoload
+     *
+     * @return ClassLoader
+     */
+    public function autoload(): ClassLoader
+    {
+        return require __DIR__ . '/vendor/autoload.php';
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized(): void
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main events we are interested in
+        $this->enable([
+            // Put your main events here
+        ]);
+    }
+
+    public function onFormProcessed(Event $event): void
+    {
+        $form = $event['form'];
+        $action = $event['action'];
+        $params = $event['params'];
+
+        $vars = [
+            'form' => $form,
+            'page' => $this->grav['page']
+        ];
+
+        if ($action === 'mailersend') {
+            $params = $this->processParams($params, $vars);
+            $api_token = $this->config->get('plugins.mailersend.api_token');
+            $mailersend = new MailerSend(['api_key' => $api_token]);
+            $emailParams = new EmailParams();
+
+            $recipients = $this->processRecipients('to', $params);
+            $emailParams->setRecipients($recipients);
+            $emailParams->setCc($this->processRecipients('cc', $params));
+            $emailParams->setBcc($this->processRecipients('bcc', $params));
+
+            $emailParams->setSubject($params['subject'] ?? $this->grav['page']->title() ?? $this->config->get('site.title'));
+
+            $from_all = $this->processRecipients('from', $params);
+            if (is_array($from_all)) {
+                $from = array_shift($from_all)->toArray();
+                $emailParams->setFrom($from['email'])
+                            ->setFromName($from['name']);
+            }
+
+            if (isset($params['reply_to'])) {
+                $reply_to = $this->createRecipient($params['reply_to'])->toArray();
+                $emailParams->setReplyTo($reply_to['email'])
+                            ->setReplyToName($reply_to['name']);
+            }
+
+            if (isset($params['template_id'])) {
+                $to = array_shift($recipients)->toArray();
+                $vars = $params['substitutions'] ?? [];
+                $variables = [new Variable($to['email'], $vars)];
+                $emailParams->setTemplateId($params['template_id'])->setVariables($variables);
+
+            } else {
+                $message = $params['body'] ?? $params['message'] ?? $form->value('message') ?? '';
+                $html = (bool) ($params['html'] ?? false);
+                if ($html) {
+                    $converter = new HtmlConverter();
+                    $html = html_entity_decode($message);
+                    $text = $converter->convert($html);
+                    $emailParams->setHtml($html);
+                    $emailParams->setText($text);
+                } else {
+                    $emailParams->setText($message);
+                }
+            }
+
+            try {
+                $mailersend->email->send($emailParams);
+            } catch (MailerSendException | JsonException | ClientExceptionInterface $e) {
+                $this->grav['log']->error("Mailersend: " . $e->getMessage());
+            }
+        }
+    }
+
+    protected function processRecipients(string $type, array $params)
+    {
+        $recipients = $params[$type] ??
+                      $this->config->get('plugins.mailersend.defaults.'.$type) ??
+                      $this->config->get('plugins.email.'.$type) ??
+                      [];
+
+        $list = [];
+
+        if (!empty($recipients)) {
+            if (is_array($recipients) && Utils::isAssoc($recipients)) {
+                $list[] = $this->createRecipient($recipients);
+            } else {
+                if (is_array($recipients[0])) {
+                    foreach ($recipients as $recipient) {
+                        $list[] = $this->createRecipient($recipient);
+                    }
+                } else {
+                    $list[] = $this->createRecipient($recipients);
+                }
+            }
+        }
+
+        return $list;
+    }
+
+    protected function createRecipient($data): Recipient
+    {
+        if (is_string($data)) {
+            $email = $data;
+            $name = null;
+        } elseif (Utils::isAssoc($data)) {
+            $email = $data['email'] ?? $data['mail'] ?? $data['address'] ?? null;
+            $name = $data['name'] ?? null;
+        } else {
+            $email = $data[0] ?? null;
+            $name = $data[1] ?? null;
+        }
+        return new Recipient($email, $name);
+    }
+
+    protected function processParams(array $params, array $vars = []): array
+    {
+        $twig = $this->grav['twig'];
+        array_walk_recursive($params, function(&$value) use ($twig, $vars) {
+            $value = $twig->processString($value, $vars);
+        });
+        return $params;
+    }
+}
